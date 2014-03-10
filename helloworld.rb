040ae0e1bd4054a9688f5853e5268b2ce7c2872c
deartrudence <trudy.macnabb@gmail.com>
2014-03-10 19:49:30 -0400

    added ordered list

--- helloworld.rb
+++ helloworld.rb
@@ -3,5 +3,10 @@ require 'rubygems'
 
 get '/' do
   "<h1 id='unique'>Heading</h1>
-  <p>Hello World!</p>"
+  <p>Hello World!</p>
+  <ol>
+  <li>Trudy</li>
+  <li>Jenninfer</i>
+  <li>Macnabb</i>
+  </ol>"
 end
