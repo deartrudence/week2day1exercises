3fa15deba89200f12003a42ef2ca2d699cb60cc4
deartrudence <trudy.macnabb@gmail.com>
2014-03-10 21:08:18 -0400

    added some views

--- helloworld.rb
+++ helloworld.rb
@@ -2,16 +2,16 @@ require 'sinatra'
 require 'rubygems'
 
 get '/' do
-  "<style>
-  		p {color: blue;}
-  		.done {text-decoration:line-through;}
-  		li:hover {background: grey;}
-   </style>
-  <h1 id='unique'>Heading</h1>
-  <p>Hello World!</p>
-  <ol>
-  <li class='done'>Trudy</li>
-  <li>Jenninfer</i>
-  <li>Macnabb</i>
-  </ol>"
+  erb :index
 end
+
+get '/hello/:name' do
+	name = params[:name]
+
+	erb :hello, locals: {
+		some_name: name,
+		x: Time.now
+	}
+
+end
+
